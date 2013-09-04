194aec5c9e15fef7bcbb8d9068f95065832b2465
crebstar <crebstar.paul@yahoo.com>
2013-09-04 16:31:59 -0500

    Test Commit on Mac OSX

--- Classes/HelloWorldScene.cpp
+++ Classes/HelloWorldScene.cpp
@@ -1,5 +1,7 @@
 #include "HelloWorldScene.h"
 
+#include <string>
+
 USING_NS_CC;
 
 CCScene* HelloWorld::scene()
@@ -73,7 +75,7 @@ bool HelloWorld::init()
     // add the sprite as a child to this layer
     this->addChild(pSprite, 0);
 
-
+    
 	CCSprite * testSprite = CCSprite::create("TestSprite.png");
 	testSprite->setPosition( ccp(-300, -300));
 	CCMoveTo * mov = CCMoveTo::create( 7.0f, ccp(visibleSize.width, visibleSize.height));
